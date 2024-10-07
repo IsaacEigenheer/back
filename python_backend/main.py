2fffc86d41fd2ee373ef234a278c64a429bc4a67
Isaac Loureiro Eigenheer <eigenheeri@brascabos.intranet>
2024-10-07 14:09:06 -0300

    adicionando env gitignore e excluindo imagens main.py

--- python_backend/main.py
+++ python_backend/main.py
@@ -70,6 +70,34 @@ def start(path, config, current_client):
 
     make_finalSheet(current_client, filename_id)
 
+    if os.path.exists('cropped_images'):
+        for file in os.listdir('cropped_images'):
+            if filename_id in file:
+                os.remove(f'cropped_images/{file}')
+    
+    if os.path.exists('./crops2'):
+        for file in os.listdir('./crops2'):
+            if filename_id in file:
+                os.remove(f'./crops2/{file}')
+
+    if os.path.exists('Excel'):
+        for file in os.listdir('Excel'):
+            if filename_id in file:
+                if 'planilha_final' not in file:
+                    os.remove(os.path.join('Excel', file))
+
+    
+    if os.path.exists('processed_images'):
+        for file in os.listdir('processed_images'):
+            if filename_id in file:
+                os.remove(os.path.join('processed_images', file))
+
+    if os.path.exists('images'):
+        for file in os.listdir('images'):
+            if filename_id in file:
+                os.remove(os.path.join('images', file))
+
+
     print('Finish!', flush=True)
 
 
